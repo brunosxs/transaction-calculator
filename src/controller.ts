81374e5c107c18629dc56e7b7ea33700338e4ece
BrunoSXS <trustmeiamadeveloper@gmail.com>
2019-08-30 23:20:37 -0300

    clean up the controller methods

--- src/controller.ts
+++ src/controller.ts
@@ -1,35 +1,30 @@
-import { Request, Response, NextFunction } from "express";
-import DataService from "./service/dataService";
-import Transaction from "./model/Transaction";
+import { Request, Response } from 'express';
+import DataService from './service/dataService';
+import Transaction from './model/Transaction';
 
 export class Controller {
-
-    static async helloWorld(req: Request, res: Response, next: NextFunction) {
-        return res.send("Hello from the api")
+    static async helloWorld(req: Request, res: Response): Promise<Response> {
+        return res.send('Hello from the api');
     }
 
-    static async merchantDiscountRate(req: Request, res: Response, next: NextFunction) {
-
-        res.status(200).json(DataService.getInstance().getAll(true))
+    static async merchantDiscountRate(req: Request, res: Response): Promise<Response> {
+        return res.status(200).json(DataService.getInstance().getAll(true));
     }
 
-    static async transaction(req: Request, res: Response, next: NextFunction) {
-
+    static async transaction(req: Request, res: Response): Promise<Response> {
         const { Valor, Adquirente, Bandeira, Tipo } = req.body;
         const adquirente = DataService.getInstance().getByAdquirente(Adquirente);
         let mdr = 0;
         const taxa = adquirente.Taxas.filter(t => {
-            return t.Bandeira.toLowerCase() === Bandeira
-        })[0]
+            return t.Bandeira.toLowerCase() === Bandeira;
+        })[0];
 
-
-        if (Tipo === "credito") {
+        if (Tipo === 'credito') {
             mdr = taxa.Credito;
-        } else if (Tipo === "debito") {
+        } else if (Tipo === 'debito') {
             mdr = taxa.Debito;
         }
         const transaction = new Transaction(Adquirente, Valor, mdr);
-        res.status(200).send({ "ValorBruto": transaction.calculateValorBruto() })
-
+        return res.status(200).send({ ValorBruto: transaction.calculateValorBruto() });
     }
 }
