cb60b538cf893a0947b3b1e58b9811844fc6aa9f
BrunoSXS <trustmeiamadeveloper@gmail.com>
2019-08-30 23:21:50 -0300

    added negative test for Valor
    added test for Valor = 0

--- src/controller.spec.ts
+++ src/controller.spec.ts
@@ -13,4 +13,16 @@ describe('/transaction', () => {
             .send({ Valor: 100, Adquirente: 'A', Bandeira: 'visa', Tipo: 'debito' });
         expect(result.body).toEqual({ ValorBruto: 98 });
     });
+    it('Fail with 0 as the Valor number', async () => {
+        const result = await supertest(app)
+            .post('/transaction')
+            .send({ Valor: 0, Adquirente: 'A', Bandeira: 'visa', Tipo: 'debito' });
+        expect(result.error.status).toEqual(422);
+    });
+    it('Fail with negative Valor number', async () => {
+        const result = await supertest(app)
+            .post('/transaction')
+            .send({ Valor: -100, Adquirente: 'A', Bandeira: 'visa', Tipo: 'debito' });
+        expect(result.error.status).toEqual(422);
+    });
 });
